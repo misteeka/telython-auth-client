02f58a47a91b9a6bbc0394f08b6473367ebe2b41
misteeka <83140665+misteeka@users.noreply.github.com>
2022-04-11 13:00:07 +0300

    Performance improved

--- client.go
+++ client.go
@@ -1,67 +1,131 @@
-package github
+package tauth
 
 import (
 	"fmt"
-	transport "github.com/misteeka/fasthttp"
+	"github.com/valyala/fasthttp"
+	"strconv"
+	"time"
 )
 
-type Response []byte
+type Status int
 
 var (
-	SUCCESS               = Response{100}
-	INVALID_REQUEST       = Response{101}
-	INTERNAL_SERVER_ERROR = Response{102}
-	AUTHORIZATION_FAILED  = Response{103}
-	ALREADY_EXISTS        = Response{104}
-	NOT_FOUND             = Response{105}
+	SUCCESS               Status = 100
+	INVALID_REQUEST       Status = 101
+	INTERNAL_SERVER_ERROR Status = 102
+	AUTHORIZATION_FAILED  Status = 103
+	ALREADY_EXISTS        Status = 104
+	NOT_FOUND             Status = 105
 )
 
-func get(function string) ([]byte, error) {
-	resp, err := transport.Get("http://127.0.0.1:8001/auth/" + function)
-	if err != nil {
-		return nil, err
+var client fasthttp.HostClient
+
+func init() {
+	client = fasthttp.HostClient{
+		Addr:                "127.0.0.1:8001",
+		MaxIdleConnDuration: time.Minute,
+		ReadTimeout:         30 * time.Second,
+		WriteTimeout:        30 * time.Second,
 	}
-	response := resp.Body()
-	transport.ReleaseResponse(resp)
-	return response, nil
-}
-func post(function string, json string) ([]byte, error) {
-	resp, err := transport.Post("http://127.0.0.1:8001/auth/"+function, []byte(json))
-	if err != nil {
-		return nil, err
-	}
-	response := resp.Body()
-	transport.ReleaseResponse(resp)
-	return response, nil
-}
-func put(function string, json string) ([]byte, error) {
-	resp, err := transport.Put("http://127.0.0.1:8001/auth/"+function, []byte(json))
-	if err != nil {
-		return nil, err
-	}
-	response := resp.Body()
-	transport.ReleaseResponse(resp)
-	return response, nil
 }
 
-func SignIn(username string, password string) (Response, error) {
+func get(function string) (Status, error) {
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI("http://127.0.0.1:8001/auth/" + function)
+	resp := fasthttp.AcquireResponse()
+	err := client.Do(req, resp)
+	fasthttp.ReleaseRequest(req)
+	if err != nil {
+		return 0, err
+	}
+	response := resp.Body()
+	ReleaseResponse(resp)
+	i, err := strconv.Atoi(string(response))
+	if err != nil {
+		return 0, err
+	}
+	return Status(i), nil
+}
+func post(function string, json string) (Status, error) {
+	req := fasthttp.AcquireRequest()
+	req.SetBody([]byte(json))
+	req.Header.SetMethod("POST")
+	req.Header.SetContentType("application/json")
+	req.SetRequestURI("http://127.0.0.1:8001/auth/" + function)
+	resp := fasthttp.AcquireResponse()
+	err := client.Do(req, resp)
+	if err != nil {
+		return 0, err
+	}
+	response := resp.Body()
+	ReleaseResponse(resp)
+	i, err := strconv.Atoi(string(response))
+	if err != nil {
+		return 0, err
+	}
+	return Status(i), nil
+}
+func put(function string, json string) (Status, error) {
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI("http://127.0.0.1:8001/auth/" + function)
+	req.Header.SetContentType("application/json")
+	req.Header.SetMethodBytes([]byte("PUT"))
+	req.SetBody([]byte(json))
+	resp := fasthttp.AcquireResponse()
+	err := client.Do(req, resp)
+	fasthttp.ReleaseRequest(req)
+	if err != nil {
+		return 0, err
+	}
+	response := resp.Body()
+	ReleaseResponse(resp)
+	i, err := strconv.Atoi(string(response))
+	if err != nil {
+		return 0, err
+	}
+	return Status(i), nil
+}
+func delete(function string, json string) (Status, error) {
+	req := fasthttp.AcquireRequest()
+	req.SetRequestURI("http://127.0.0.1:8001/auth/" + function)
+	req.Header.SetContentType("application/json")
+	req.Header.SetMethodBytes([]byte("DELETE"))
+	req.SetBody([]byte(json))
+	resp := fasthttp.AcquireResponse()
+	err := client.Do(req, resp)
+	if err != nil {
+		return 0, err
+	}
+	response := resp.Body()
+	ReleaseResponse(resp)
+	i, err := strconv.Atoi(string(response))
+	if err != nil {
+		return 0, err
+	}
+	return Status(i), nil
+}
+func ReleaseResponse(response *fasthttp.Response) {
+	fasthttp.ReleaseResponse(response)
+}
+
+func SignIn(username string, password string) (Status, error) {
 	return put("signIn", fmt.Sprintf(`{"username":"%s", "password":"%s"}`, username, password))
 }
-func CheckPassword(username string, password string) (Response, error) {
+func CheckPassword(username string, password string) (Status, error) {
 	return get(fmt.Sprintf("checkPassword?u=%s&p=%s", username, password))
 }
-func ResetPassword(username string, oldPassword string, newPassword string) (Response, error) {
+func ResetPassword(username string, oldPassword string, newPassword string) (Status, error) {
 	return put("resetPassword", fmt.Sprintf(`{"username":"%s", "oldPassword":"%s", "newPassword":"%s"}`, username, oldPassword, newPassword))
 }
-func RequestSignUpCode(username string, email string) (Response, error) {
+func RequestSignUpCode(username string, email string) (Status, error) {
 	return post("requestSignUpCode", fmt.Sprintf(`{"username":"%s", "email":"%s"}`, username, email))
 }
-func RequestPasswordRecovery(username string) (Response, error) {
+func RequestPasswordRecovery(username string) (Status, error) {
 	return put("requestPasswordRecovery", fmt.Sprintf(`{"username":"%s"}`, username))
 }
-func RecoverPassword(username string, newPassword string, code string) (Response, error) {
+func RecoverPassword(username string, newPassword string, code string) (Status, error) {
 	return put("recoverPassword", fmt.Sprintf(`{"username":"%s", "newPassword":"%s", "code":"%s"}`, username, newPassword, code))
 }
-func SignUp(username string, password string, code string) (Response, error) {
+func SignUp(username string, password string, code string) (Status, error) {
 	return post("signUp", fmt.Sprintf(`{"username":"%s", "password":"%s", "code":"%s"}`, username, password, code))
 }
